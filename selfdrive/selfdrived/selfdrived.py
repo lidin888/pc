644af7ca521007908a80d338390d24055c3b232b
lidin888 <lidin888@github.com>
2025-12-01 15:55:39 +0800

    feat(selfdrived): 使用LongitudinalPersonalityMax参数动态计算驾驶性格循环

--- selfdrive/selfdrived/selfdrived.py
+++ selfdrive/selfdrived/selfdrived.py
@@ -401,7 +401,8 @@ class SelfdriveD:
     # decrement personality on distance button press
     #if self.CP.openpilotLongitudinalControl:
     #  if any(not be.pressed and be.type == ButtonType.gapAdjustCruise for be in CS.buttonEvents):
-    #    self.personality = (self.personality - 1) % 3
+    #    longitudinalPersonalityMax = self.params.get_int('LongitudinalPersonalityMax')
+    #    self.personality = (self.personality - 1 + longitudinalPersonalityMax) % longitudinalPersonalityMax
     #    self.params.put_nonblocking('LongitudinalPersonality', str(self.personality))
     #    self.events.add(EventName.personalityChanged)
 
