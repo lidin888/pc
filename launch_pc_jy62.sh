0f4f33ca01f49d8166c8f71cad67c3c471b29201
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-12-07 09:44:11 +0800

    修改兼容/dev/video*以及/dev/v4l/by-path/*的摄像头设备路径

--- launch_pc_jy62.sh
+++ launch_pc_jy62.sh
@@ -4,7 +4,7 @@ sudo udevadm control --reload-rules
 sudo udevadm trigger
 sudo chmod 777 /dev/bus/usb/*
 # 运行op
-cd /data/openpilot && 
-source .venv/bin/activate && 
-USE_WEBCAM=1 ROAD_CAM=0 WIDE_CAM=2 NO_DM=0 JY62=1 system/manager/manager.py
+cd /data/openpilot &&
+source .venv/bin/activate &&
+USE_WEBCAM=1 USE_CAMPATH=1 ROAD_CAM=0 WIDE_CAM=2 NO_DM=0 JY62=1 system/manager/manager.py
 
