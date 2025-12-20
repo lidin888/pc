81e21850651e8006886d44eac1582f018ad6a780
lidin888 <lidin888@github.com>
2025-12-20 11:35:32 +0800

    fix: 修复pandad.py中DEPRECATED_DEVICES属性错误
    - 将不存在的DEPRECATED_DEVICES替换为F4_DEVICES
    - F4_DEVICES包含已弃用的设备类型(WHITE_PANDA, BLACK_PANDA等)

--- selfdrive/pandad/pandad.py
+++ selfdrive/pandad/pandad.py
@@ -38,7 +38,7 @@ def flash_panda(panda_serial: str) -> Panda:
 
   # skip flashing if the detected device is deprecated from upstream
   hw_type = panda.get_type()
-  if hw_type in Panda.DEPRECATED_DEVICES:
+  if hw_type in Panda.F4_DEVICES:
     cloudlog.warning(f"Panda {panda_serial} is deprecated (hw_type: {hw_type}), skipping flash...")
     return panda
 
