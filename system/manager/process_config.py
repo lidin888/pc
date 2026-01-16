f1b258b4247a5ec65a6efcf525575d214c006fdc
openpilot <5604101+axi82@user.noreply.gitee.com>
2026-01-16 22:23:37 +0800

    process_config.py:增加车道检测服务端代码的启动

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -144,6 +144,7 @@ procs = [
   #PythonProcess("fleet_manager", "selfdrive.frogpilot.fleetmanager.fleet_manager", check_fleet, enabled=not PC),
   PythonProcess("fleet_manager", "selfdrive.frogpilot.fleetmanager.fleet_manager", check_fleet),
   PythonProcess("carrot_man", "selfdrive.carrot.carrot_man", always_run),#, enabled=not PC),
+  PythonProcess("lane", "selfdrive.carrot.lane", only_onroad),
 ]
 
 #if Params().get_int("ComputerType") == 1:
